41e02298d26ad972fd6a0ea158e2b518075447a9
AbduRasul21 <al.rasul.0404@gmail.com>
2023-01-24 16:28:49 +0500

    end part 18

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -19,15 +20,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let posts = [];
+let requestTitle = "";
 
 app.get("/", function (req, res) {
   res.render("home", {
     startingContent: homeStartingContent,
     posts: posts,
   });
-
-    
-
 });
 app.get("/about", function (req, res) {
   res.render("about", { aboutContent: aboutContent });
@@ -42,12 +41,26 @@ app.get("/compose", function (req, res) {
 
 app.post("/compose", function (req, res) {
   const post = {
-    postTitle: req.body.postTitle,
-    postBody: req.body.postBody,
+    title: req.body.postTitle,
+    content: req.body.postBody,
   };
   posts.push(post);
   console.log(posts);
   res.redirect("/");
+});
+
+app.get("/posts/:postName", (req, res) => {
+  requestTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.title);
+    if (requestTitle === storedTitle) {
+      console.log("Matches");
+      console.log(requestTitle + storedTitle);
+    } else {
+      console.log("not matches");
+    }
+  });
 });
 
 app.listen(3000, function () {
